eacb0ea6d4db2628200884ec8a4a343affa4d1bd
Srikanth Miriyala <srikanthvikas@gmail.com>
2018-04-08 10:54:08 -0400

    Added click package to Shotty. Gathered more instance information

--- shotty.py
+++ shotty.py
@@ -1,9 +1,22 @@
 import boto3
+import click
+
+sess = boto3.session
+ec2_sess = sess.boto3.resource('ec2')
+
+@click.command()
+def list_instances():
+        "List EC2 instances"
+        for k in ec2_sess.instances.all():
+            print (','.join((
+                k.id,
+                k.instance_type,
+                k.placement['AvailabilityZone'],
+                k.state['Name'],
+                k.public_dns_name
+                )))
+
+        return
 
 if __name__=='__main__':
-    sess = boto3.session
-
-    ec2_sess = sess.boto3.resource('ec2')
-
-    for k in ec2_sess.instances.all():
-        print k
+    list_instances()
