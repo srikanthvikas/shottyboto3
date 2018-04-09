5cfdee6d99f49e56a314b8012e1ac46263a82732
Srikanth Miriyala <srikanthvikas@gmail.com>
2018-04-08 23:18:54 -0400

    List instances by project filter. Start, stop and list instances

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Access EC2 servers using BOTO3 library
 pipenv steps
 
 ```
-pipenv run python shotty.py
+pipenv run python shotty.py <command> <--project=PROJECT>
 ```
+
+*command* is list, start, stop
+*project* is optional
--- shotty.py
+++ shotty.py
@@ -4,19 +4,67 @@ import click
 sess = boto3.session
 ec2_sess = sess.boto3.resource('ec2')
 
-@click.command()
-def list_instances():
+def filter_instances(project):
+        instances=[]
+
+        if project:
+            filters = [{'Name':'tag:Project','Values':[project]}]
+            instances = ec2_sess.instances.filter(Filters=filters)
+        else:
+            instances = ec2_sess.instances.all()
+
+        return instances
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+@instances.command('list')
+@click.option('--project',default=None,
+    help="Only instances for Project (tag Project:<name>)")
+
+def list_instances(project):
         "List EC2 instances"
-        for k in ec2_sess.instances.all():
+        instances=filter_instances(project)
+
+        for k in instances:
+            tags = { t['Key']:t['Value'] for t in k.tags or [] }
+
             print (','.join((
                 k.id,
                 k.instance_type,
                 k.placement['AvailabilityZone'],
                 k.state['Name'],
-                k.public_dns_name
+                k.public_dns_name,
+                tags.get('Project', '<no project>')
                 )))
-
         return
+@instances.command('stop')
+@click.option('--project', default=None, help='Only instances for Project')
+
+def stop_instances(project):
+    "Stop Ec2 instances"
+
+    instances=filter_instances(project)
+
+    for i in instances:
+        print ('stopping {0}...' .format(i.id))
+        i.stop()
+    return
+
+@instances.command('start')
+@click.option('--project', default=None, help='Only instances for Project')
+
+def stop_instances(project):
+    "Start Ec2 instances"
+
+    instances=filter_instances(project)
+
+    for i in instances:
+        print ('starting {0}...' .format(i.id))
+        i.start()
+    return
+
 
 if __name__=='__main__':
-    list_instances()
+    instances()
